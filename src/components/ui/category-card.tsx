5bf49824037b0708bce5325eb9fe4c9b1445cdde
Maryammmy <maryamsameh1610@gmail.com>
2026-02-18 23:25:08 +0200

    category card

--- src/components/ui/category-card.tsx
+++ src/components/ui/category-card.tsx
@@ -27,13 +27,13 @@ export default function CategoryCard({ item }: CategoryCardProps) {
           <p className="mt-1 text-xs text-white/80">{item.subtitle}</p>
         </div>
 
-        <div className="absolute inset-0 z-999 translate-y-full bg-bg-creamy p-6 text-dark opacity-0 shadow-sm transition-all duration-400 ease-out group-hover:translate-y-0 group-hover:opacity-100">
+        <div className="absolute inset-0 z-10 translate-y-full bg-bg-creamy p-6 text-dark opacity-0 shadow-sm transition-all duration-400 ease-out group-hover:translate-y-0 group-hover:opacity-100">
           {/* <div className="absolute left-5 top-6 h-8 w-8 rounded-tl-lg border-l-2 border-t-2 border-primary/40" /> */}
           <div className="absolute right-5 top-6 h-8 w-8 rounded-tr-lg border-r-2 border-t-2 border-primary/40" />
           <div className="absolute left-5 bottom-6 h-8 w-8 rounded-bl-lg border-b-2 border-l-2 border-primary/40" />
           {/* <div className="absolute right-5 bottom-6 h-8 w-8 rounded-br-lg border-b-2 border-r-2 border-primary/40" /> */}
           <div className="flex items-center gap-2">
-            <div className=" h-1 w-10 rounded-full bg-primary" />
+            <div className="h-1 w-10 rounded-full bg-primary" />
             <span className="text-[10px] font-semibold uppercase tracking-[0.2em] text-primary">
               Discover
             </span>
